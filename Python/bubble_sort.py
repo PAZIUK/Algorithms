a34c70b35d539b62e5f9e59307c4356841515b31
Serhii Paziuk <71930843+PAZIUK@users.noreply.github.com>
2022-12-26 14:06:36 +0200

    Update bubble_sort.py

--- Python/bubble_sort.py
+++ Python/bubble_sort.py
@@ -3,8 +3,8 @@ not_sorted = [5, 7, 4, 3, 8, 2]
 
 def bubble_sort(array):
     for run in range(len(array) - 1):
-        for i in range(len(array) - run - 1):
-            if array[i] > array[i + 1]:
+        for i in range(len(array) - run - 1):                   # Search maximum value (Bubble)
+            if array[i] > array[i + 1]:                         # Value exchange
                 array[i], array[i + 1] = array[i + 1], array[i]
     return array
 
