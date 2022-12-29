c96b4e92459e5993eebdc7154c927774ca43a6b1
serhiy <paz.serhiy@gmail.com>
2022-12-29 09:05:37 +0200

    Add fixed and tested binary search algorithm

--- Python/binary_search.py
+++ Python/binary_search.py
@@ -1,19 +1,29 @@
-my_array = [1, 3, 5, 7, 9]
+import math
+
+int_array = [1, 3, 5, 7, 9]
+str_array = ['Bogdan', 'Dionisii', 'Oleksandr', 'Serhii', 'Stanislav']
 
 
-def binary_search(array, item):
-    low = 0
-    high = len(array) - 1
+def binary_search(array, item):             # Number binary search
+
+    low = 0                                 # Low limit
+    high = len(array) - 1                   # High limit
+
     while low <= high:
-        mid = (low + high)
-        guess = array[mid]
-        if guess == item:
-            return mid
-        if guess > item:
-            high = mid - 1
-        else:
-            low = mid + 1
-    return None
+        mid = math.ceil((low + high) / 2)   # Get middle element index
+        guess = array[mid]                  # Get middle element
+
+        if guess == item:                   # Value found
+            return mid                      # Return value index
+
+        if guess > item:                    # Much
+            high = mid - 1                  # Change high limit
+
+        else:                               # Less
+            low = mid + 1                   # Change low limit
+
+    return None                             # Value doesn't exist
 
 
-print(binary_search(my_array, 3))
+print(binary_search(int_array, 3))
+print(binary_search(str_array, 'Serhii'))
