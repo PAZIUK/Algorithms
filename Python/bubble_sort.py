da37ee87a7dac494adaf31980c7eb92f57a7e59d
serhiy <paz.serhiy@gmail.com>
2023-01-07 15:49:36 +0200

    Add some changes and add quick sort

--- Python/bubble_sort.py
+++ Python/bubble_sort.py
@@ -1,7 +1,7 @@
 not_sorted = [5, 7, 4, 3, 8, 2]
 
 
-def bubble_sort(array):                                         # Sort (min -> max)
+def bubble_sort(array: list) -> list:                           # Sort (min -> max)
     for run in range(len(array) - 1):
         for i in range(len(array) - run - 1):                   # Search max value (Bubble)
             if array[i] > array[i + 1]:                         # Value exchange
@@ -9,7 +9,7 @@ def bubble_sort(array):                                         # Sort (min -> m
     return array
 
 
-def bubble_sort_reverse(array):                                 # Reverse sort (max -> min)
+def bubble_sort_reverse(array: list) -> list:                   # Reverse sort (max -> min)
     for run in range(len(array) - 1):
         for i in range(len(array) - run - 1):                   # Search min value (Bubble)
             if array[i] < array[i + 1]:                         # Value exchange
--- Python/selection_sort.py
+++ Python/selection_sort.py
@@ -1,7 +1,7 @@
 not_sorted = [-3, 5, 0, -8, 1, 10]
 
 
-def selection_sort(array):                                      # Sort (min -> max)
+def selection_sort(array: list) -> list:                        # Sort (min -> max)
     for run in range(len(array) - 1):
         for i in range(run + 1, len(array)):                    # Search min value
             if array[i] < array[run]:
@@ -10,7 +10,7 @@ def selection_sort(array):                                      # Sort (min -> m
     return array
 
 
-def selection_sort_reverse(array):                              # Sort (max -> min)
+def selection_sort_reverse(array: list) -> list:                # Sort (max -> min)
     for run in range(len(array) - 1):
         for i in range(run + 1, len(array)):                    # Search max value
             if array[i] > array[run]:
